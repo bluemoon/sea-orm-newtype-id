4ad0ef1e32cc97fe245252bbcd567d66eabc9821
Bradford Toney <bradford.toney@gmail.com>
2022-12-07 16:05:48 -0500

    fix: readme

--- newtype_id/src/lib.rs
+++ newtype_id/src/lib.rs
@@ -1,3 +1,11 @@
+/// # Sea-orm Newtype ID is a ID generation system 🐚
+
+/// - 🎲 Currently uses `nanoid`
+/// - 🦓 Stripe style IDs allow for users to see what kind of IDs are being used without having to log them in the database
+
+/// ## Rationale
+
+/// - `String`ly typed IDs can be very error prone, think of the case where you are using more than one ID at a time
 pub use sea_orm_newtype_id_domain::*;
 pub use sea_orm_newtype_id_macros::def_id;
 
